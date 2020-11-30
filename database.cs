4b66b808f11aad0e3b73373e57bc07e2d42c0e87
phoals <56293439+phoals@users.noreply.github.com>
2020-11-30 16:24:20 +0300

    Create database.cs

---
+++ database.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Database
+    {
+        public List<Product> products = new List<Product>();
+    }
+}
