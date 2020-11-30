7f43466ca7b182d17b410d774c89654b0ad53600
phoals <56293439+phoals@users.noreply.github.com>
2020-11-30 16:24:43 +0300

    Create basket.cs

---
+++ basket.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Basket
+    {
+        public List<Product> products = new List<Product>();
+    }
+}
