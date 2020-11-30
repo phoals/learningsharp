9dce6070eb32c7e86cf24b89157b853f6e0d4ec5
phoals <56293439+phoals@users.noreply.github.com>
2020-11-30 16:23:55 +0300

    Create products.cs

---
+++ products.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Product
+    {
+        public string name;
+        public int price;
+    }
+}
